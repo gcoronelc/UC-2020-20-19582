65d740852c18586792759bd0458da5a19cbb660f
Gustavo Coronel <gcoronelc@gmail.com>
2021-08-19 12:19:53 -0500

    Semana 1

---
+++ Semana01/Ejercicio1-Lincolcpp.cpp
@@ -0,0 +1,15 @@
+/*
+ * Lincol Paolo Astuhuaman Huiñucana11:43
+*/
+#include<iostream>
+using namespace std;
+int main ()
+{
+int r,area;
+setlocale (LC_CTYPE, "Spanish");
+cout<<"Ingrese el radio del circulo: ";
+cin>>r;
+area=3.1416*r*r;
+cout<<"El area del circulo es: "<<area;
+
+}
---
+++ Semana01/Ejercicio1-Wilfredo.cpp
@@ -0,0 +1,15 @@
+/*
+ * WILFREDO PAUL LAZARO CARPIO11:43
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+	int radio;
+	cout<<"Ingrese Radio: ";
+	cin>>radio;
+	float area=radio*radio*3.14;
+	cout<<"El area del circulo  es: "<<area;
+		
+	return 0;
+}
