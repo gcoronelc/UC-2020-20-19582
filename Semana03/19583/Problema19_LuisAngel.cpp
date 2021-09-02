bb0673529bfad087cf19cef863da199dce7bc64b
Gustavo Coronel <gcoronelc@gmail.com>
2021-09-02 12:11:59 -0500

    Problema 08

---
+++ Semana03/19583/Problema19_LuisAngel.cpp
@@ -0,0 +1,46 @@
+// LUIS ANGEL CONDORI RAMON12:09
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	// Configuracion
+	setlocale(LC_CTYPE,"Spanish");
+	
+	// Variables
+	int sueld;
+	float s_n,s_n1,s_n2,des,des1,des2;	
+	
+	// Lectura de datos
+	cout<<"Ingrese su sueldo \t\t"; cin>>sueld;
+	
+	// Proceso y Reporte
+	if( sueld>=0 && sueld<=1000)
+	{
+		cout<<"Descuento: 10% = "<<des<< endl;
+		des=sueld*0.1;
+		s_n=sueld-(des);
+		cout<<"Sueldo Neto:"<<s_n<< endl;
+	}
+	else if( sueld>1000 && sueld<=2000)
+	{
+		cout<<"Descuento: 15% = "<<des1<< endl;
+		des1=sueld*0.15;
+		s_n1=sueld-(des1);
+		cout<<"Sueldo Neto:"<<s_n1<< endl;
+	}
+	else if( sueld>2000)
+	{
+		cout<<"Descuento: 20% = "<<des2<< endl;
+		des2=sueld*0.2;
+		s_n2=sueld-(des2);
+		cout<<"Sueldo Neto:"<<s_n2<< endl;
+	}
+	else
+	{
+		cout<<"Ingreso un sueldo incorrecto \n";
+	}
+	// Fin
+	system("pause");
+	return 0;
+}
