8506a64cef2925ee71b7135bff96e8f8ae6f91e9
Gustavo Coronel <gcoronelc@gmail.com>
2021-10-13 13:30:12 -0500

    Semana 09

---
+++ Semana09/Ejemplo01.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int sumar(int num1, int num2){
+	int suma;
+	// Proceso: Inicio
+	suma = num1 + num2;
+	// Proceso: Fin
+	return suma; // Retorna la suma
+}
+
+int main(){
+	
+	// Configuracion
+	setlocale(LC_CTYPE,"Spanish");
+	
+	cout << "7 + 5 = " << sumar(7,5) << endl;
+	
+	// Fin
+	system("pause");
+	return 0;
+}
