3e8cb9152c4fbf552edaca8bb15db59a5b1983dd
Gustavo Coronel <gcoronelc@gmail.com>
2021-10-14 13:55:44 -0500

    Potencia

--- Semana09/NRC-19584/Problema04.cpp
+++ Semana09/NRC-19584/Problema04.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 using namespace std;
 
+int fnPotencia (int base, int exponente){
+	int potencia;
+	// Proceso: Inicio
+	potencia = 1;
+	for(int i=1; i<=exponente; i++){
+		potencia *= base;
+	}
+	// Proceso: Fin
+	return potencia;
+}
+
 double fnPromedio (int n1, int n2, int n3){
 	double promedio;
 	// Proceso : Inicio
@@ -36,7 +47,7 @@ int main(){
 	
 	// Variables
 	int numero1, numero2, numero3;
-	int mayor, menor;
+	int mayor, menor, potencia;
 	double promedio;
 	
 	// Lectura de datos
@@ -51,6 +62,7 @@ int main(){
 	menor = fnMenor(numero1, numero2, numero3);
 	mayor = fnMayor(numero1, numero2, numero3);
 	promedio = fnPromedio(numero1, numero2, numero3);
+	potencia = fnPotencia(numero1, numero2);
 	
 	// Reporte
 	cout << endl;
@@ -59,6 +71,7 @@ int main(){
 	cout << "Número menor: " << menor << endl;
 	cout << "Número mayor: " << mayor << endl;
 	cout << "Promedio: " << promedio << endl;
+	cout << "Potencia n1^n2: " << potencia << endl;
 	cout << endl;
 	
 	// Fin
