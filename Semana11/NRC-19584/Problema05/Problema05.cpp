9a60593ad29f17504897992ec0d9d794e78b1a2d
Gustavo Coronel <gcoronelc@gmail.com>
2021-10-28 13:35:07 -0500

    Semana 11

--- Semana11/NRC-19584/Problema05/Problema05.cpp
+++ Semana11/NRC-19584/Problema05/Problema05.cpp
@@ -3,6 +3,12 @@
 #include "Utilitarios.h"
 using namespace std;
 
+// Funciones y procedimiento
+void procRegistrarVenta();
+void procListado();
+void procReporte();
+void procSalir();
+
 int main(){
 	
 	// Configuracion
@@ -11,11 +17,26 @@ int main(){
 	int opMenu;
 	// Proceso principal
 	do{
+		// Menu
 		mostrarCabecera();
 		procMostrarMenu();
 		opMenu = leerOpcion(1,4);
-		
-		cout << "OP = " << opMenu << endl;
+		// Procesar opción del menú
+		mostrarCabecera();
+		switch(opMenu){
+			case 1:
+				procRegistrarVenta();
+				break;
+			case 2:
+				procListado();
+				break;
+			case 3:
+				procReporte();
+				break;
+			case 4:
+				procSalir();
+				break;
+		}
 		// Fin
 		if(opMenu!=4){
 			system("pause");
@@ -26,6 +47,26 @@ int main(){
 	return 0;
 }
 
+// Funciones y procedimiento
+void procRegistrarVenta(){
+	cout << "En construcción" << endl;
+	cout << endl;
+}
 
+void procListado(){
+	cout << "En construcción" << endl;
+	cout << endl;
+}
+
+void procReporte(){
+	cout << "En construcción" << endl;
+	cout << endl;	
+}
+
+void procSalir(){
+	cout << "Fin del programa" << endl;
+	cout << "Vuelva pronto" << endl;
+	cout << endl;	
+}
 
 
